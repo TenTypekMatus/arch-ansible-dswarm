af30b4abf92901d80e557775f0cd2adc345ddfa2
Matus Mastena (TenTypekMatus) <Shadiness9530@proton.me>
2023-03-26 12:06:58 +0200

    Updates

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i hosts.ini site.yml -kK
-ansible-playbook -i hosts.ini ./dswarm/deploy-swarm.yml -kK
+ansible-playbook -i hosts.ini deploy.yml -kK
