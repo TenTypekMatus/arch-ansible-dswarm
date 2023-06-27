6c7b8fa96f3f5ef5b938fb84f142a28dcbab55cf
Matus Mastena <Shadiness9530@proton.me>
2023-06-27 11:16:43 +0200

    We don't need un scipt anymore

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ansible-galaxy install robertdebock.update geerlingguy.security geerlingguy.pip geerlingguy.glusterfs artis3n.tailscale geerlingguy.docker thomasjpfan.docker-swarm mrlesmithjr.manage-lvm 
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts.ini site.yml -K --ask-vault-pass
